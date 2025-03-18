2942c49d2ced207c402ef6d077ace395748c388e
Antonio Perez <antonio.perez@jala.university>
2025-03-17 20:12:26 -0600

    Add ports to route and cors

--- src/main/java/com/tec/api/gateway/demo/config/SecurityConfig.java
+++ src/main/java/com/tec/api/gateway/demo/config/SecurityConfig.java
@@ -1,21 +1,51 @@
 package com.tec.api.gateway.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configuración de CORS
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // Desactivar CSRF
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/files/**", "/auth/**").permitAll()
+                        .pathMatchers("/files/**", "/auth/**").permitAll() // Permitir acceso sin autenticación
                         .anyExchange().authenticated()
-                )
-                .csrf(csrf -> csrf.disable());
+                );
         return http.build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(allowedOrigins));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
